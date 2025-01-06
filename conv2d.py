60389406a0460d07bd9f0cde78a6e870d8831fc8
MehceUnisen <mehceu@gmail.com>
2025-01-07 02:06:07 +0300

    quantized the input in to shape of filter, no explicit loops btw ://

--- conv2d.py
+++ conv2d.py
@@ -28,6 +28,13 @@ class Conv2d():
         size=(output_channel))
     
   def pad(self, x):
+    self.input_dimensions = {
+      'B':x.shape[0],
+      'C':x.shape[1],
+      'W':x.shape[2],
+      'H':x.shape[3],
+    }
+
     B, C, W, H = x.shape[0], x.shape[1], x.shape[2], x.shape[3]
     S, K = self.kwargs['stride'], self.kwargs['kernel_size']
     padding = ceil(((W - 1) * S + 1 - H + (K - 1)) / 2)
@@ -42,16 +49,19 @@ class Conv2d():
     _conv_coords = np.stack([np.repeat(__kernel_tl_points, len(__kernel_tl_points), axis=0), 
                  np.repeat(__kernel_tl_points.reshape(1, -1), len(__kernel_tl_points),
                  axis=0).flatten()], axis=1)
+    
     return _conv_coords
-
-  def forward(self, x):
-    x = self.pad(x) if self.kwargs['padding'] else x # padded
+    
+  def forward(self, X):
+    X = self.pad(X) if self.kwargs['padding'] else X # padded
     S, K = self.kwargs['stride'], self.kwargs['kernel_size']
 
-    _conv_coords = self.__get_filter_top_left_corner(x) 
-    print(_conv_coords)
-    # do matrix-wise multiplication for each coord in _conv_coords
+    _conv_coords = self.__get_filter_top_left_corner(X) 
+    __conv_range_x = _conv_coords[:, 0][:, np.newaxis] + np.arange(K, dtype=np.int8) 
+    __conv_range_y = _conv_coords[:, 1][:, np.newaxis] + np.arange(K, dtype=np.int8) 
 
+    a = X[:, :, __conv_range_x[:, :, np.newaxis], __conv_range_y[:, np.newaxis, :]]
+    
   def backward(self, loss):
     pass
 
@@ -60,4 +70,13 @@ class Conv2d():
   
 
 l1 = Conv2d(kernel_size=3, padding=0, stride=2)
-l1.forward(np.arange(30 * 24 ** 2).reshape(10,3,24, 24))
+l1.forward(np.arange(30 * 7 ** 2).reshape(10,3,7,7))
+
+# # # # # # # 
+# # # # # # # 
+# # # # # # # 
+# # # # # # # 
+# # # # # # # 
+# # # # # # # 
+# # # # # # # 
+
