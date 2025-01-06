2e84fdd6b2e4403d8096cd06b16596f0014a6864
MehceUnisen <mehceu@gmail.com>
2025-01-06 21:05:41 +0300

    padding and stacking done

---
+++ conv2d.py
@@ -0,0 +1,58 @@
+import numpy as np
+from math import floor, ceil
+class Conv2d():
+  def __init__(self, 
+               kernel_size = 3,
+               input_channel = 3, 
+               output_channel = 64,
+               stride = 1, 
+               padding = 0):
+
+    assert(kernel_size >= 0 and "Kernel size must be positive")
+    assert(input_channel >= 0 and "Input channel must be positive")
+    assert(output_channel >= 0 and "Output channel must be positive")
+    assert(stride > 0 and 'Stride must be positive')
+    assert((padding == 0 or padding == 1) and 'Padding must be 0 | 1')
+
+    self.kwargs = {
+    'kernel_size' : kernel_size,
+    'input_channel' : input_channel,
+    'output_channel' : output_channel,
+    'stride' : stride,
+    'padding' : padding
+    }
+
+    self.filter = np.random.normal(loc=.0, scale=.4,
+        size=(output_channel, kernel_size, kernel_size))
+    self.bias = np.random.normal(loc=.0, scale=.4,
+        size=(output_channel))
+    
+  def pad(self, x):
+    B, C, W, H = x.shape[0], x.shape[1], x.shape[2], x.shape[3]
+    S, K = self.kwargs['stride'], self.kwargs['kernel_size']
+    padding = ceil(((W - 1) * S + 1 - H + (K - 1)) / 2)
+    P_W, P_H = W + 2 * padding, H + 2 * padding
+    return np.pad(
+            x.reshape(B * C, W, H), padding)[padding:-padding].reshape(B, C, P_W, P_H)
+
+    
+
+  def forward(self, x):
+    x = self.pad(x) if self.kwargs['padding'] else x # padded
+    S, K = self.kwargs['stride'], self.kwargs['kernel_size']
+
+    __kernel_tl_points = np.arange(0, x.shape[-1] - K + S, S)
+    _conv_coords = np.stack([np.repeat(__kernel_tl_points, len(__kernel_tl_points), axis=0), 
+                 np.repeat(__kernel_tl_points.reshape(1, -1), len(__kernel_tl_points),
+                 axis=0).flatten()], axis=1)
+    # do matrix-wise multiplication for each coord in _conv_coords
+
+  def backward(self, loss):
+    pass
+
+  def call(self, x):
+    pass
+  
+
+l1 = Conv2d(kernel_size=3, padding=0, stride=2)
+l1.forward(np.arange(30 * 24 ** 2).reshape(10,3,24, 24))
